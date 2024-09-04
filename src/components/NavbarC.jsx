3dec94c9eee4517d51ca86803cc01ed73fae8dad
unknown <mateox463@gmail.com>
2024-09-04 14:57:31 -0300

    Se actualiza Navbar

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -9,14 +9,20 @@ const NavbarC = () => {
       <Navbar expand="lg" className="bg-color-nav">
         <Container className="">
           <Navbar.Brand href="#home">
-            <img src="/img/logoejemplo.jpeg" alt="" />
+            <img
+              src="../public/img/logoejemplo1.jpeg"
+              alt="logo del gimnasio"
+              width={"150px"}
+            />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+            <Nav className="me-auto">
               <Nav.Link href="#home">Inicio</Nav.Link>
               <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
               <Nav.Link href="#link">Contacto</Nav.Link>
+            </Nav>
+            <Nav>
               <Nav.Link href="#link">Iniciar Sesion</Nav.Link>
               <Nav.Link href="#link">Registrarse</Nav.Link>
             </Nav>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
