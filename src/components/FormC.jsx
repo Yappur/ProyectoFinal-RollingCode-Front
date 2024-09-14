e91d827d477aad71d91a180168565c0e1fac17f8
unknown <mateox463@gmail.com>
2024-09-14 01:03:08 -0300

    se añaden estilos de prueba para Register Page

--- src/components/FormC.jsx
+++ src/components/FormC.jsx
@@ -3,6 +3,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { FaUser, FaLock } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
+import "../css/ComponentsCSS/FormC.css";
 const FormC = (idPagina) => {
   const [formRegister, setFormRegister] = useState({});
   const [formLogin, setFormLogin] = useState({});
@@ -11,45 +12,69 @@ const FormC = (idPagina) => {
   const handleChangeRegister = (ev) => {
     setFormRegister({ ...formRegister, [ev.target.name]: ev.target.value });
   };
+
+  const handleClickRegister = () => {
+    ev.preventDefault();
+    console.log(formRegister);
+    const { nombre, gmail, contrasenia, repetirContrasenia } = formRegister;
+  };
+
   return (
     <div className="d-flex justify-content-center ">
       <Form>
         <Form.Group className="mb-3" controlId="formGroupText">
-          <Form.Label>Nombre</Form.Label> <FaUser />
-          <Form.Control type="text" placeholder="Nombre" className="d-flex" />
-        </Form.Group>
-        <Form.Group
-          className="mb-3"
-          controlId="formGroupEmail"
-          onChange={handleChangeRegister}
-        >
-          <Form.Label>Gmail</Form.Label> <IoMdMail />
+          <Form.Label>Nombre</Form.Label>
+          <FaUser className="icon" />
           <Form.Control
+            name="nombre"
+            type="text"
+            placeholder="Nombre"
+            className="boxForm"
+            onChange={handleChangeRegister}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Gmail</Form.Label>
+          <IoMdMail className="icon" />
+          <Form.Control
+            name="gmail"
             type="email"
             placeholder="Gmail"
+            className="boxForm"
             onChange={handleChangeRegister}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Contraseña</Form.Label>
-          <FaLock />
+          <FaLock className="icon" />
+
           <Form.Control
+            name="contrasenia"
             type="password"
             placeholder="Contraseña"
+            className="boxForm"
             onChange={handleChangeRegister}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupRepetirContrasenia">
           <Form.Label>Repetir Contraseña</Form.Label>
-          <FaLock />
+          <FaLock className="icon" />
+
           <Form.Control
+            name="repetirContrasenia"
             type="password"
             placeholder="Repetir Contraseña"
+            className="boxForm"
             onChange={handleChangeRegister}
           />
         </Form.Group>
 
-        <Button className="w-100  " variant="primary" type="submit">
+        <Button
+          className="w-100  "
+          variant="primary"
+          type="submit"
+          onClick={handleClickRegister}
+        >
           Enviar
         </Button>
       </Form>
