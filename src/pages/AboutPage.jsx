f448329d509d97f5e9ebb52987a92ed10409f632
santiago590 <sanllapur@gmail.com>
2024-09-21 17:22:11 -0300

    se Actualiza el About Page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,17 @@
+import React, { useState } from "react";
+import "../css/PagesCSS/AboutPage.css";
 const AboutPage = () => {
-  return <div>holamundo</div>;
+  return (
+    <div className="container-about">
+      <h2>Sobre Nosotros</h2>
+      <p>
+        En <strong>[Nombre del Gimnasio]</strong>, creemos que cada persona
+        tiene objetivos y necesidades únicas. Ofrecemos un enfoque personalizado
+        que se adapta a cada uno de nuestros miembros, ya sea que busques perder
+        peso, ganar músculo o mejorar tu condición física.
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
