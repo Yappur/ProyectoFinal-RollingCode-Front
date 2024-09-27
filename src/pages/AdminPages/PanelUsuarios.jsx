f5c41ae12c3879d0b12a0248758e6c9ee8eeb912
unknown <mateox463@gmail.com>
2024-09-27 15:19:13 -0300

    se añade propiedades de usuarios a la tabla

--- src/pages/AdminPages/PanelUsuarios.jsx
+++ src/pages/AdminPages/PanelUsuarios.jsx
@@ -1,8 +1,32 @@
 import { cambiarTituloPagina } from "../../helpers/cambiarTitulos";
+import Container from "react-bootstrap/Container";
+import TableC from "../../components/TableC";
+import "../../css/PagesCSS/PanelUsuarios.css";
+import { useState, useEffect } from "react";
 
 const PanelUsuarios = () => {
   cambiarTituloPagina("PanelUsuarios");
-  return <div>PanelUsuarios</div>;
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  const obtenerUsuarios = () => {
+    const usuariosLs = JSON.parse(localStorage.getItem("usuarios")) || [];
+    setUsuarios(usuariosLs);
+  };
+
+  useEffect(() => {
+    obtenerUsuarios();
+  }, []);
+  return (
+    <>
+      <div className="container-admin-table">
+        <h2>Panel de administrador</h2>
+        <Container className="container-table">
+          <TableC dataUsers={usuarios} idPagina={"usuarios"} />
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default PanelUsuarios;
