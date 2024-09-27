f725722be5ff868e36ea2c4783a7871840b6bca4
unknown <mateox463@gmail.com>
2024-09-27 15:53:59 -0300

    se añade paginacion de la table de usuarios

--- src/pages/AdminPages/PanelUsuarios.jsx
+++ src/pages/AdminPages/PanelUsuarios.jsx
@@ -3,11 +3,14 @@ import Container from "react-bootstrap/Container";
 import TableC from "../../components/TableC";
 import "../../css/PagesCSS/PanelUsuarios.css";
 import { useState, useEffect } from "react";
+import Pagination from "react-bootstrap/Pagination";
 
 const PanelUsuarios = () => {
   cambiarTituloPagina("PanelUsuarios");
 
   const [usuarios, setUsuarios] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Página actual
+  const [itemsPerPage] = useState(8); // Número de usuarios por página
 
   const obtenerUsuarios = () => {
     const usuariosLs = JSON.parse(localStorage.getItem("usuarios")) || [];
@@ -17,13 +20,55 @@ const PanelUsuarios = () => {
   useEffect(() => {
     obtenerUsuarios();
   }, []);
+
+  const indexOfLastUser = currentPage * itemsPerPage;
+  const indexOfFirstUser = indexOfLastUser - itemsPerPage;
+  const currentUsers = usuarios.slice(indexOfFirstUser, indexOfLastUser);
+
+  // Cambiar la página
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPages = Math.ceil(usuarios.length / itemsPerPage);
+  const paginationItems = [];
+  for (let number = 1; number <= totalPages; number++) {
+    paginationItems.push(
+      <Pagination.Item
+        key={number}
+        active={number === currentPage}
+        onClick={() => handlePageChange(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
   return (
     <>
       <div className="container-admin-table">
-        <h2>Panel de administrador</h2>
+        <h2 className="d-flex justify-content-center align-items-center">
+          Panel de administrador
+        </h2>
         <Container className="container-table">
-          <TableC dataUsers={usuarios} idPagina={"usuarios"} />
+          <TableC dataUsers={currentUsers} idPagina={"usuarios"} />
         </Container>
+        <div className="d-flex justify-content-center align-items-center">
+          <Pagination>
+            <Pagination.First onClick={() => setCurrentPage(1)} />
+            <Pagination.Prev
+              onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)}
+            />
+
+            {paginationItems}
+
+            <Pagination.Next
+              onClick={() =>
+                currentPage < totalPages && setCurrentPage(currentPage + 1)
+              }
+            />
+            <Pagination.Last onClick={() => setCurrentPage(totalPages)} />
+          </Pagination>
+        </div>
       </div>
     </>
   );
