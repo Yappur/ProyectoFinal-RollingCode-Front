a78e4d2e27fe00b17bd16be1c8b78a26f30150b9
unknown <mateox463@gmail.com>
2024-09-27 01:50:02 -0300

    se crea un css y se añaden botones al homeAdminPage

--- src/pages/HomeAdminPage.jsx
+++ src/pages/HomeAdminPage.jsx
@@ -1,8 +1,31 @@
 import { cambiarTituloPagina } from "../helpers/cambiarTitulos";
+import { Button } from "react-bootstrap";
+import "../css/PagesCSS/HomeAdminPage.css";
 
 const HomeAdminPage = () => {
   cambiarTituloPagina("HomeAdminPage");
-  return <div>HomeAdminPage</div>;
+  return (
+    <>
+      <div className="botones-admin">
+        <div className="mb-2">
+          <Button variant="info" size="lg">
+            Panel Usuarios
+          </Button>{" "}
+          <Button variant="info" size="lg">
+            Panel Clases
+          </Button>
+        </div>
+        <div className="mb-2">
+          <Button variant="outline-warning" size="lg">
+            Vista Previa Home
+          </Button>{" "}
+          <Button variant="outline-warning" size="lg">
+            Vista Previa HomeUser
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomeAdminPage;
