4e147d0914cfdd0eb024b2026a017c2e9a555733
unknown <mateox463@gmail.com>
2024-09-28 22:58:34 -0300

    se crea componente de tabla de beneficios

--- src/pages/HomeUserPage.jsx
+++ src/pages/HomeUserPage.jsx
@@ -4,6 +4,8 @@ import RedesSociales from "../components/RedesSocialesC";
 import WhatsappContact from "../components/WhatsappContact";
 import { useNavigate } from "react-router-dom";
 import "../css/PagesCSS/HomeUserPage.css";
+import { Table } from "react-bootstrap";
+import TableBeneficiosC from "../components/TableBeneficiosC";
 const HomeUserPage = () => {
   cambiarTituloPagina("HomeUserPage");
 
@@ -17,12 +19,9 @@ const HomeUserPage = () => {
       <div>
         <div className="bienvenida-usuario">
           <h1 className="scale-up-center">
-            Bienvenido a <span className="h1-titulo">EnerGYM</span>
+            Conoce los Beneficios de nuestro Gimnasio.
           </h1>
-          <p>Rompe tus límites, no tus sueños.</p>
-
-          <Button variant="outline-light">Empieza Hoy!</Button>
-          {""}
+          <TableBeneficiosC />
         </div>
         <div className="imagenTurnos">
           <Button variant="outline-light" onClick={handleClick}>
