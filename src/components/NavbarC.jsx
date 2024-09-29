812bc2537dfa4c54913bf88992eeff05351889dc
unknown <mateox463@gmail.com>
2024-09-28 22:25:56 -0300

    se añade nuevas condicionales al navbar

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -54,9 +54,19 @@ const NavbarC = () => {
               <Nav.Link onClick={handleHomeClick}>Inicio</Nav.Link>
               <Nav.Link href="/planes">Planes</Nav.Link>
               <Nav.Link href="/turnero">Galeria</Nav.Link>
-              <Nav.Link href="/about">Sobre Nosotros</Nav.Link>
-              <Nav.Link href="/contact">Contacto</Nav.Link>
+              {!usuarioLogueado && (
+                <>
+                  <Nav.Link href="/about">Sobre Nosotros</Nav.Link>
+                  <Nav.Link href="/contact">Contacto</Nav.Link>
+                </>
+              )}
+              {usuarioLogueado && usuarioLogueado.role === "usuario" && (
+                <>
+                  <Nav.Link href="/turnero">Turnos</Nav.Link>
+                </>
+              )}
             </Nav>
+
             <Nav className="ms-auto">
               {usuarioLogueado ? (
                 <Nav.Link href="/" onClick={handleLogout}>
