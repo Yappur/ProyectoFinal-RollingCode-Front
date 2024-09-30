d1554d634ed885c2a24907c2654eab10bbc64edc
unknown <mateox463@gmail.com>
2024-09-29 23:39:05 -0300

    se añade texto de Bienvenida en HomeUserPage

--- src/pages/HomeUserPage.jsx
+++ src/pages/HomeUserPage.jsx
@@ -4,8 +4,8 @@ import RedesSociales from "../components/RedesSocialesC";
 import WhatsappContact from "../components/WhatsappContact";
 import { useNavigate } from "react-router-dom";
 import "../css/PagesCSS/HomeUserPage.css";
-import { Table } from "react-bootstrap";
 import TableBeneficiosC from "../components/TableBeneficiosC";
+import UbicacionC from "../components/UbicacionC";
 
 const HomeUserPage = () => {
   cambiarTituloPagina("HomeUserPage");
@@ -20,10 +20,14 @@ const HomeUserPage = () => {
       <div>
         <div className="bienvenida-usuario">
           <h1 className="scale-up-center">
-            Conoce los Beneficios de nuestro Gimnasio.
+            ¡Gracias por registrarte en nuestro Gimnasio!
           </h1>
+          <h2 className="scale-up-center">
+            Conoce los Beneficios de <span className="h1-titulo">EnerGYM</span>.
+          </h2>
           <TableBeneficiosC />
         </div>
+
         <div className="imagenTurnos">
           <Button variant="outline-light" onClick={handleClick}>
             Saca tu Turno{" "}
