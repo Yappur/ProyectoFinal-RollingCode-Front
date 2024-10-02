355f47d1acd219e884252f33210a2337c26ee868
unknown <mateox463@gmail.com>
2024-10-01 21:36:25 -0300

    se añade hero a galeryPage con sus estilos

--- src/pages/InstalacionesPage.jsx
+++ src/pages/InstalacionesPage.jsx
@@ -1,6 +1,7 @@
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import "../css/PagesCSS/Instalaciones.css";
+import BotonC from "../components/BotonC";
 import { cambiarTituloPagina } from "../helpers/cambiarTitulos";
 
 const InstalacionesPage = () => {
@@ -59,6 +60,19 @@ const InstalacionesPage = () => {
 
   return (
     <div className="container-instalaciones">
+      <div className="hero-instalaciones">
+        <div className="hero-instalaciones-componentes">
+          <h1>GALERIA</h1>
+          <BotonC to={"/planes"} children={"Hazte socio"} />
+        </div>
+      </div>
+      <div className="text-instalaciones">
+        <h2>ENERGYM EN IMÁGENES</h2>
+        <p>
+          Dale un vistazo a nuestras instalaciones y únete a la gran familia de
+          EnerGym, tu gimnasio en Tucuman.
+        </p>
+      </div>
       <div className="gallery-container">
         <ImageGallery items={galeriaImagenes} showPlayButton={false} />
       </div>
