7bc0d108c27867aae9e054870cee9a14dc7fb98b
unknown <mateox463@gmail.com>
2024-12-13 23:11:19 -0300

    se añade principios de carrousel infinito de logos

---
+++ src/components/HomeComponents/InfiniteCarousel.jsx
@@ -0,0 +1,20 @@
+import "../../css/ComponentsCSS/InfiniteCarousel.css";
+
+const InfiniteCarousel = () => {
+  return (
+    <div class="logos">
+      <div class="logos-slide">
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142001/logo-Kappa-640x360_l6wtvd.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142000/logo-Puma-500x249_uym4go.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142000/logo-Fila-500x281_lpj9oq.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142000/Logo-Nike-1-500x239_bbggbg.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142000/logo-Asics-500x166_jveafl.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142000/logo-Champion-500x281_vwdctr.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734141999/logo-Umbro-500x278_wcnv5r.png" />
+        <img src="https://res.cloudinary.com/doh6efk57/image/upload/v1734142000/New-Balance-logo-500x282_w81fa4.png" />
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteCarousel;
