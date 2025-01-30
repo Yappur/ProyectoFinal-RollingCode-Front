248ccda782b100a945e504cc514476fb7e5c5222
unknown <mateox463@gmail.com>
2025-01-29 21:22:46 -0300

    se añade pagina panelturnos a navbar

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -73,6 +73,9 @@ const NavbarC = () => {
                   <NavLink to="/admin/clases" className={"nav-link"}>
                     Panel Productos
                   </NavLink>
+                  <NavLink to="/admin/turnos" className={"nav-link"}>
+                    Panel Turnos
+                  </NavLink>
                 </>
               ) : (
                 <>
