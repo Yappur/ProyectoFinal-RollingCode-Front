1e2c0f5c102739f1d7ead6f6bd5d5c5c92e4308a
unknown <mateox463@gmail.com>
2025-01-30 22:08:26 -0300

    se añaden principios de panel turnos

--- src/pages/AdminPages/PanelTurnos.jsx
+++ src/pages/AdminPages/PanelTurnos.jsx
@@ -1,7 +1,161 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import TableTurnos from "./TableTurnos";
+import Pagination from "react-bootstrap/Pagination";
+import clientAxios, { configHeaders } from "../../helpers/axios.config";
+import Swal from "sweetalert2";
 
 const PanelTurnos = () => {
-  return <div>PanelTurnos</div>;
+  const [turnos, setTurnos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(8);
+
+  const obtenerTurnos = async () => {
+    try {
+      const result = await clientAxios.get(
+        "/turnos/listaTurnos",
+        configHeaders
+      );
+      if (Array.isArray(result.data)) {
+        setTurnos(result.data);
+      } else {
+        console.error("El formato de datos no es el esperado:", result.data);
+      }
+    } catch (error) {
+      console.error("Error al obtener turnos:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Error al cargar los turnos",
+      });
+    } finally {
+      setIsLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    obtenerTurnos();
+  }, []);
+
+  const eliminarTurno = async (id) => {
+    try {
+      const result = await Swal.fire({
+        title: "¿Estás seguro?",
+        text: "¡No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminar!",
+      });
+
+      if (result.isConfirmed) {
+        await clientAxios.delete(`/turnos/${id}`, configHeaders);
+        setTurnos((prevTurnos) =>
+          prevTurnos.filter((turno) => turno._id !== id)
+        );
+        Swal.fire({
+          title: "¡Eliminado!",
+          text: "El turno ha sido eliminado.",
+          icon: "success",
+        });
+      }
+    } catch (error) {
+      console.error("Error al eliminar el turno:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Error al eliminar el turno",
+      });
+    }
+  };
+
+  const actualizarTurno = async (updatedTurno) => {
+    try {
+      const response = await clientAxios.put(
+        `/turnos/${updatedTurno._id}`,
+        updatedTurno,
+        configHeaders
+      );
+
+      if (response.data) {
+        setTurnos((prevTurnos) =>
+          prevTurnos.map((turno) =>
+            turno._id === updatedTurno._id ? response.data.turno : turno
+          )
+        );
+
+        Swal.fire({
+          icon: "success",
+          title: "¡Éxito!",
+          text: "El turno ha sido actualizado correctamente",
+        });
+      }
+    } catch (error) {
+      console.error("Error al actualizar el turno:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.response?.data?.mensaje || "Error al actualizar el turno",
+      });
+    }
+  };
+
+  // Paginación
+  const indexOfLastTurno = currentPage * itemsPerPage;
+  const indexOfFirstTurno = indexOfLastTurno - itemsPerPage;
+  const currentTurnos = turnos.slice(indexOfFirstTurno, indexOfLastTurno);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPages = Math.ceil(turnos.length / itemsPerPage);
+  const paginationItems = [];
+  for (let number = 1; number <= totalPages; number++) {
+    paginationItems.push(
+      <Pagination.Item
+        key={number}
+        active={number === currentPage}
+        onClick={() => handlePageChange(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  return (
+    <div className="body-turnos">
+      <div className="container-turno-text">
+        <h2 className="d-flex justify-content-center align-items-center">
+          Panel de administración de turnos
+        </h2>
+      </div>
+      <Container className="container-table">
+        <TableTurnos
+          dataItems={currentTurnos}
+          eliminarItem={eliminarTurno}
+          actualizarTurno={actualizarTurno}
+        />
+      </Container>
+      <div className="d-flex justify-content-center align-items-center">
+        <Pagination>
+          <Pagination.First onClick={() => setCurrentPage(1)} />
+          <Pagination.Prev
+            onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)}
+          />
+          {paginationItems}
+          <Pagination.Next
+            onClick={() =>
+              currentPage < totalPages && setCurrentPage(currentPage + 1)
+            }
+          />
+          <Pagination.Last onClick={() => setCurrentPage(totalPages)} />
+        </Pagination>
+      </div>
+    </div>
+  );
 };
 
 export default PanelTurnos;
