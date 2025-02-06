3409898824a3676aa70d13a0172a86ecaddba264
unknown <mateox463@gmail.com>
2025-02-06 20:52:06 -0300

    Fix: se arregla icono de ubicacion que no era visible en deploy

--- src/components/UbicacionC.jsx
+++ src/components/UbicacionC.jsx
@@ -1,31 +1,37 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "../css/ComponentsCSS/UbicacionC.css";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "../css/ComponentsCSS/UbicacionC.css";
+
+const customIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 const UbicacionC = () => {
   return (
-    <>
-      <div className="contenedor-mapa">
-        <MapContainer
-          center={[-26.82414, -65.2226]}
-          zoom={16}
-          scrollWheelZoom={false}
-          className="mapa"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[-26.82414, -65.2226]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
-    </>
+    <div className="contenedor-mapa">
+      <MapContainer
+        center={[-26.82414, -65.2226]}
+        zoom={16}
+        scrollWheelZoom={false}
+        className="mapa"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-26.82414, -65.2226]} icon={customIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
