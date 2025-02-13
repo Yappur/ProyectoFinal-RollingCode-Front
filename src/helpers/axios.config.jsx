0c50bde533762a1edfeece0cd642776198410670
unknown <mateox463@gmail.com>
2025-02-13 12:51:16 -0300

    se actualiza axios.config

--- src/helpers/axios.config.jsx
+++ src/helpers/axios.config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL_LOCAL,
+  baseURL: import.meta.env.VITE_BACKEND_URL_DEPLOY,
 });
 
 clientAxios.interceptors.request.use(
